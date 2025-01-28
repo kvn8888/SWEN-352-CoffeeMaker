5717aaaa6c644ee360d4e74a79b2b735d3fd3c3c
pwc9501 <pwc9501@g.rit.edu>
2025-01-28 15:17:45 -0500

    Add basic tests for Recipe name, price, and ingredient amounts

--- src/test/java/coffeemaker/RecipeTest.java
+++ src/test/java/coffeemaker/RecipeTest.java
@@ -9,21 +9,57 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RecipeTest {
-    private Recipe recipe;
+    private Recipe CuT;
 
     @BeforeEach
     public void setUp() {
-
+        CuT = new Recipe();
     }
 
     @AfterEach
     public void tearDown() {
-
+        CuT = null;
     }
 
     @Test
     @DisplayName("Sanity Check")
-    public void testRecipe() {
+    public void sanityCheck() {
         assertTrue(true, "This should always pass");
+    }
+
+    @Test
+    @DisplayName("Check Name")
+    public void checkName() {
+        assertNotNull(CuT.getName(), "Testing Recipe Name");
+    }
+
+    @Test
+    @DisplayName("Check Price")
+    public void checkPrice() {
+        assertTrue(CuT.getPrice() > -1, "Testing Recipe Price");
+    }
+
+    @Test
+    @DisplayName("Check Coffee Amount")
+    public void checkCoffeeAmount() {
+        assertTrue(CuT.getAmtCoffee() > -1, "Testing Recipe Coffee Amount");
+    }
+
+    @Test
+    @DisplayName("Check Milk Amount")
+    public void checkMilkAmount() {
+        assertTrue(CuT.getAmtMilk() > -1, "Testing Recipe Milk Amount");
+    }
+
+    @Test
+    @DisplayName("Check Sugar Amount")
+    public void checkSugarAmount() {
+        assertTrue(CuT.getAmtSugar() > -1, "Testing Recipe Sugar Amount");
+    }
+
+    @Test
+    @DisplayName("Check Chocolate Amount")
+    public void checkChocolateAmount() {
+        assertTrue(CuT.getAmtChocolate() > -1, "Testing Recipe Chocolate Amount");
     }
 }
