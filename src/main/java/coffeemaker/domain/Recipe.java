f7ee88f49b27a05629854e4ecd5f88cebfc720c4
pwc9501 <pwc9501@g.rit.edu>
2025-01-30 16:50:44 -0500

    remove unreachable code in Recipe.hashCode()

--- src/main/java/coffeemaker/domain/Recipe.java
+++ src/main/java/coffeemaker/domain/Recipe.java
@@ -180,10 +180,7 @@ public class Recipe {
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    return result;
+    return name.hashCode();
   }
 
   @Override
